92da71419cf5ec08e9d6e84092bbd86e5bc1987e
charukant <its4cs@gmail.com>
2018-06-18 06:22:14 +0530

    Do it

---
+++ Assignments/Linux/ShellScripting/Day1Sol2.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+clear
+
+# Input your name into a variable
+
+printf '\n\tWhat you like to be called as? : ' 
+# read myName
+
+# Print the required result on console
+
+echo -e '\n\t'$1		' , Hello'
+printf '\n\t Hello , '$1'\n\n\t Hello , '$1'\n\n\t How r u?\n\n\n'
+
+
+
