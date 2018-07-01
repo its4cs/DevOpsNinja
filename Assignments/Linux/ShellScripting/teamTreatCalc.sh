c8be02aa493a18be867b9ae84fb8fb51ed2b5b5f
charukant <its4cs@gmail.com>
2018-07-01 12:54:28 +0530

    Do it

---
+++ Assignments/Linux/ShellScripting/teamTreatCalc.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Initialize global variables or store the essential data variables in a masterData file
+# Number of team members
+# Price of item to purchase
+# Names of all team members stored with an index number.
+# Contribution amount = (Price of item to purchase) *  (total team members)
+# Late fine range 1-5 -- LFR
+# - MinVal
+# - MaxVal
+# % for LFR1-5
+
+clear
+
+printf '\n\tWhat is the price of item decided for treat : '
+read myName
+
+# Print the required result on console
+
+echo -e '\n\t'$myName		' , Hello'
+printf '\n\t Hello , '$myName'\n\n\t Hello , '$myName'\n\n\t How r u?\n\n\n'
+
+
+
